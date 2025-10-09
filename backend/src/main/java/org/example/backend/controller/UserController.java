061885ea2bb53b70c921b7125c7b84a11994bca2
malek.b. <malek.boulakbech@gmail.com>
2025-10-09 11:17:15 +0200

    add Dockerfile

--- backend/src/main/java/org/example/backend/controller/UserController.java
+++ backend/src/main/java/org/example/backend/controller/UserController.java
@@ -1,9 +1,15 @@
 package org.example.backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+    @GetMapping
+    public String getName(){
+        return "Hello World";
+    }
 }
